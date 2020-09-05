f40834ff4e9f67e36cfb6fad1b402633b973a0ad
inatoff <inatoff@gmail.com>
2020-09-05 18:22:42 +0300

    Add rest exception to details

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using Domain;
 using MediatR;
 using Persistence;
@@ -27,7 +28,6 @@ namespace Application.Activities
             public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activity =  await _context.Activities.FindAsync(request.Id);
-
                 if (activity == null) throw new RestException(HttpStatusCode.NotFound, new { activity = "Not found"});
 
                 return activity;
