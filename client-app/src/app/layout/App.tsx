af4a5ef9b7e816fe4e8f6e3d524c579d4ee62f5a
inatoff <inatoff@gmail.com>
2020-09-15 23:38:42 +0300

    End of section 10. Adding validation

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
+import { ToastContainer } from 'react-toastify';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route, withRouter, RouteComponentProps } from 'react-router-dom';
+import { Route, withRouter, RouteComponentProps, Switch } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import NotFound from './NotFound';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
 
@@ -18,15 +20,19 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
         path={'/(.+)'}
         render={() => (
           <Fragment>
+            <ToastContainer position="bottom-right"/>
             <NavBar />
             <Container style={{ marginTop: '5em' }}>
-              <Route exact path='/activities' component={ActivityDashboard} />
-              <Route path='/activities/:id' component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={['/createActivity', '/manage/:id']}
-                component={ActivityForm}
-              />
+              <Switch>
+                <Route exact path='/activities' component={ActivityDashboard} />
+                <Route path='/activities/:id' component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={['/createActivity', '/manage/:id']}
+                  component={ActivityForm}
+                />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </Fragment>
         )}
